9a0513fd0e5096fee9afe5fabf8a74044387971a
dougdotcon <dougdotcon@gmail.com>
2026-07-23 12:29:21 +0000

    re

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/drivetube',
+  trailingSlash: true,
   env: {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
